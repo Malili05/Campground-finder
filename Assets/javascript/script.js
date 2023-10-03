aa78733c8841e98e49c836531d511ecd0cd4a386
Malisa Litten <malisalitten@gmail.com>
2023-10-02 22:08:22 -0600

    got working dropdown menu

--- Assets/javascript/script.js
+++ Assets/javascript/script.js
@@ -1,14 +1,35 @@
 // global var 
 //var npsapiRootUrl = "https://developer.nps.gov/api/v1/"
 //var npsapiKey = "s6p9WyTqkZ8EGpaLI4YUraP4LPf0BEkmFtlLC3H8"
-$(document).ready(function() {
-    $('select').formSelect();
-});
-
-
-//dom element ref
-//put the document.quertSelector for the dropdown and target it by ID. will need to 
+// $(document).ready(function() {
+//     $('select').formSelect();
+// });
 var stateCode = "UT"
+// var selectedState = document.getElementById("state-select")
+// selectedState.addEventListener("select", assignSelectedState)
+// function assignSelectedState(event){
+//     event.preventDefault()
+//     console.log("hello")
+//     console.log(event.target)
+// }
+//Grab input from search form
+let searchInput = document.getElementById('search-form');
+searchInput.addEventListener('click', getSearchInput);
+function getSearchInput(event) {
+  event.preventDefault();
+  let searchTerm = document.getElementById('search-term').value.trim();
+  let searchFormat = document.getElementById('search-format').value.trim();
+ 
+ console.log(searchTerm)
+ console.log(searchFormat)
+};
+//dom element ref
+//put the document.quertSelector for the dropdown and target it by ID. will need to
+
+
+//use bulma istead of materiaize
+
+
 // the value of state will be the user input fromt the drop down menu. we will set the value to "" after the user input will grab the state. 
 
 //function to display parks by state as selected from drop down
@@ -29,59 +50,3 @@ function getParkByState() {
             }
 
             getParkByState()
-
-// so if I need to get the drop down o show names, but I need to change the var state code to abbrevieations. I need a list of each. this list and they need to match. 
-            // var data = names();
-            // returns
-            //     [
-            //         'Alabama',
-            //         'Alaska',
-            //         'Arizona',
-            //         'Arkansas',
-            //         'California',
-            //         'Colorado',
-            //         'Connecticut',
-            //         'Delaware',
-            //         'Florida',
-            //         'Georgia',
-            //         'Hawaii',
-            //         'Idaho',
-            //         'Illinois',
-            //         'Indiana',
-            //         'Iowa',
-            //         'Kansas',
-            //         'Kentucky',
-            //         'Louisiana',
-            //         'Maine',
-            //         'Maryland',
-            //         'Massachusetts',
-            //         'Michigan',
-            //         'Minnesota',
-            //         'Mississippi',
-            //         'Missouri',
-            //         'Montana',
-            //         'Nebraska',
-            //         'Nevada',
-            //         'New Hampshire',
-            //         'New Jersey',
-            //         'New Mexico',
-            //         'New York',
-            //         'North Carolina',
-            //         'North Dakota',
-            //         'Ohio',
-            //         'Oklahoma',
-            //         'Oregon',
-            //         'Pennsylvania',
-            //         'Rhode Island',
-            //         'South Carolina',
-            //         'South Dakota',
-            //         'Tennessee',
-            //         'Texas',
-            //         'Utah',
-            //         'Vermont',
-            //         'Virginia',
-            //         'Washington',
-            //         'West Virginia',
-            //         'Wisconsin',
-            //         'Wyoming'
-            //     ]
